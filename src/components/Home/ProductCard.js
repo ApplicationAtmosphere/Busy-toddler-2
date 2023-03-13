686059539dbfc12a538f86fcfe662c6fe1e7c078
imvikaskashyap <mvikaskashyap@gmail.com>
2023-03-13 22:23:16 +0530

    responsive

--- src/components/Home/ProductCard.js
+++ src/components/Home/ProductCard.js
@@ -8,11 +8,6 @@ const ProductCard = (props) => {
 			<Card style={{ width: "inherit" }}>
 				<Card.Img variant="top" src={props.imgSrc} />
 				<Card.Body>
-					{/* <Card.Title>Card Title</Card.Title> */}
-					{/* <Card.Text>
-						Some quick example text to build on the card title and make up the
-						bulk of the card's content.
-					</Card.Text> */}
 					<div className="product-actions">
 						<p>$3000</p>
 						<Link to="/cart">
