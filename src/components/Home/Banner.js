9c277b892e6e010b9a4ff4d941cbc28e0a429d98
imvikaskashyap <mvikaskashyap@gmail.com>
2023-03-16 12:36:33 +0530

    caraousel responsive

--- src/components/Home/Banner.js
+++ src/components/Home/Banner.js
@@ -1,6 +1,9 @@
 import React from "react";
-import Carousel from "framer-motion-carousel";
+import Carousel from "react-bootstrap/Carousel";
 import "./Banner.css";
+
+// import "bootstrap/dist/css/bootstrap.css";
+// import "react-bootstrap-carousel/dist/react-bootstrap-carousel.css";
 import banner1 from "./../../assets/banner1.png";
 import banner2 from "./../../assets/banner2.png";
 import banner3 from "./../../assets/banner3.png";
@@ -8,14 +11,26 @@ import banner4 from "./../../assets/banner4.png";
 
 const Banner = () => {
 	return (
-		<div>
-			<Carousel showDots={false} className="carousel">
-				<img className="img" src={banner1} alt="banner1" />
-				<img className="img" src={banner2} alt="banner2" />
-				<img className="img" src={banner3} alt="banner3" />
-				<img className="img" src={banner4} alt="banner4" />
+		<>
+			<Carousel indicators={false} className="main_carousel">
+				<Carousel.Item interval={1000}>
+					<img className="d-block w-100 h-70" src={banner1} alt="First slide" />
+				</Carousel.Item>
+				<Carousel.Item interval={500}>
+					<img
+						className="d-block w-100 h-70"
+						src={banner2}
+						alt="Second slide"
+					/>
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="d-block w-100 h-70" src={banner3} alt="Third slide" />
+				</Carousel.Item>
+				<Carousel.Item>
+					<img className="d-block w-100 h-70" src={banner4} alt="Third slide" />
+				</Carousel.Item>
 			</Carousel>
-		</div>
+		</>
 	);
 };
 
