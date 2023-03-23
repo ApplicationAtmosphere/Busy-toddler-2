f2027529b5e651f1f90cf65a759fc4f70b520062
imvikaskashyap <mvikaskashyap@gmail.com>
2023-03-23 18:39:23 +0530

    update

--- src/components/Cart/Cart.js
+++ src/components/Cart/Cart.js
@@ -1,7 +1,7 @@
 import React from "react";
 import { useSelector, useDispatch } from "react-redux";
 import { NavLink } from "react-router-dom";
-import { addCart, delCart } from "./../../redux/action";
+import { addCart, delCart } from "../../redux/action";
 
 const Cart = () => {
 	const state = useSelector((state) => state.handleCart);
