9ef4244e89e25245c13e6a6490885bf88029dae9
imvikaskashyap <mvikaskashyap@gmail.com>
2023-03-29 11:10:40 +0530

    insta

--- src/components/Home/Instagram/Instagram.js
+++ src/components/Home/Instagram/Instagram.js
@@ -6,6 +6,9 @@ import p3 from "./../../../assets/product3.JPG";
 import p4 from "./../../../assets/product1.jpg";
 import p5 from "./../../../assets/product2.jpg";
 import p6 from "./../../../assets/product3.JPG";
+import v1 from "../../../assets/insta video/v1.mp4";
+import v2 from "../../../assets/insta video/v2.mp4";
+import v3 from "../../../assets/insta video/v3.mp4";
 // import TextUnderline from "../TextUnderline/TextUnderline";
 import insta from "../../../assets/icons8-instagram-48.png";
 
@@ -22,17 +25,20 @@ const Instagram = () => {
 			<div className="instagram_images">
 				<div className="image_div">
 					<a href="https://www.instagram.com/p/CqE99XMpNfn/" target={"_blank"}>
-						<img src={p1} alt="" />
+						{/* <img src={p1} alt="" /> */}
+						<video controls src={v1}></video>
 					</a>
 				</div>
 				<div className="image_div">
 					<a href="https://www.instagram.com/p/CpW5rJTgLsu/" target={"_blank"}>
-						<img src={p2} alt="" />
+						{/* <img src={p2} alt="" /> */}
+						<video controls src={v2}></video>
 					</a>
 				</div>
 				<div className="image_div">
 					<a href="https://www.instagram.com/p/CpJu8FrAF2z/" target={"_blank"}>
-						<img src={p3} alt="" />
+						{/* <img src={p3} alt="" /> */}
+						<video controls src={v3}></video>
 					</a>
 				</div>
 				{/* <div className="image_div">
